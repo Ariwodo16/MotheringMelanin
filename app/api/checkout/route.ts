d758a1805af4eb1a962c95dabe00f3177d28f8e2
Michael Ariwodo <106038659+Ariwodo16@users.noreply.github.com>
2026-02-28 19:29:08 -0500

    final changes

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -22,7 +22,7 @@ export async function POST(req: NextRequest) {
 
     const session = await stripe.checkout.sessions.create({
       mode: "payment",
-      payment_method_types: ["card", "afterpay_clearpay", "klarna"],
+      payment_method_types: ["card", "afterpay_clearpay", "klarna", "affirm"],
       customer_email: customerEmail || undefined,
       line_items: [
         {
